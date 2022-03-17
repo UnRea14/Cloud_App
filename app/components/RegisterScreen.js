4ee4316282a2aa2b91624a1dff1a4087388c0e22
liams <liamshani14@gmail.com>
2022-03-17 17:39:02 +0200

    need a mail server to check if the changes work

--- app/components/RegisterScreen.js
+++ app/components/RegisterScreen.js
@@ -7,7 +7,7 @@ export default function Register({navigation}){
     const [password='', setPassword] = useState()
 
     const insertUser = () => {
-        fetch('https://5af3-2a00-7c40-c690-236-896f-c444-5b31-a733.ngrok.io/register', {
+        fetch('https://ade0-2a00-7c40-c690-236-896f-c444-5b31-a733.ngrok.io/register', {
             method: 'POST',
             headers: {
               'Accept': 'application/json',
--- backend/Server.py
+++ backend/Server.py
@@ -1,16 +1,26 @@
 from flask_mysqldb import MySQL
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, url_for
+from flask_mail import Mail, Message
+from itsdangerous import URLSafeTimedSerializer, SignatureExpired
+import re
+
+REGEX = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
 
 app = Flask(__name__)
- 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'Shaniliam1404'
-app.config['MYSQL_DB'] = 'app_database'
- 
+app.config.from_pyfile('config.cfg')
+mail = Mail(app)
+s = URLSafeTimedSerializer("thisshouldbehidden!")
+
 mysql = MySQL(app)
 with app.app_context():
     db_cursor = mysql.connection.cursor()
+
+
+def checkEmail(user_email):
+    if re.fullmatch(REGEX, user_email):
+        return True
+    return False
+
 
 @app.route('/register', methods = ['POST'])
 def register():
@@ -20,12 +30,26 @@ def register():
     db_cursor = mysql.connection.cursor()
     check = db_cursor.execute("SELECT * FROM users WHERE email = %s", [user_email])
     if check:
-        return "User email already exists in system"
-    sql_command = "INSERT INTO users (name, email, password) VALUES (%s, %s, %s)"
-    values = (user_name, user_email, user_password)
-    db_cursor.execute(sql_command, values)
+        return "Email already exists in system"
+    if not checkEmail(user_email):
+        return "Email is invalid"
+    db_cursor.execute("INSERT INTO users (name, email, password) VALUES (%s, %s, %s)", [user_name, user_email, user_password])
     mysql.connection.commit()
-    return "User inserted"
+    token = s.dumps(user_email, salt="email-confirm")
+    msg = Message("Confirm Email", sender="nobody@good.com", recipients=[user_email])
+    link = url_for("confirm_email", token=token, _external=True)
+    msg.body = "Your link is {}".format(link)
+    mail.send(msg)
+    return "User registered, verify your email by the email sent to you in your email"
+
+@app.route('/confirm_email/<token>')
+def confirm_email(token):
+    try:
+        user_email = s.loads(token, salt="email-confirm", max_age=3600)  # 1 hour
+    except SignatureExpired:
+        return "<h1>The token is expired!</h1>"
+    return "<h1>The token works</h1>"
+
 
 if __name__ == "__main__":
     app.run(host="localhost", port=8080)
