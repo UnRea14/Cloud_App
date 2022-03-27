09506e22df34e8bcca42a06c369fd4828ac3f0f2
liam <liamshani14@gmail.com>
2022-03-27 14:07:49 +0300

    login

--- app/components/LoginScreen.js
+++ app/components/LoginScreen.js
@@ -1,15 +1,32 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
 
+
 export default function Login({navigation}){
     const [email='', setEmail] = useState()
     const [password='', setPassword] = useState()
+
+    const loginUser = async() => {
+        let response = await fetch('https://e909-77-137-180-250.ngrok.io/register', {
+            method: 'POST',
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+              user_email: email,
+              user_password: password
+            })
+        })
+        let json = await response.json()
+        Alert.alert('',json)
+    }
     return (
         <View style={styles.regform}>
             <Text style={styles.header}>Login to your account</Text>
             <TextInput style={styles.textinput} placeholder="Email" underlineColorAndroid={"transparent"} onChangeText={(val) => setEmail(val)}/>
             <TextInput style={styles.textinput} placeholder="Password" underlineColorAndroid={"transparent"} secureTextEntry={true} onChangeText={(val) => setPassword(val)}/>
-            <TouchableOpacity style={styles.button} onPress={console.log(email + "-" + password)}>
+            <TouchableOpacity style={styles.button} onPress={() => loginUser()}>
                 <Text style={styles.buttontext}>Login</Text>
             </TouchableOpacity>
         </View>
